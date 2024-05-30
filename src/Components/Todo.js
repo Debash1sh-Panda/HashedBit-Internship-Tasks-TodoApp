02b8410e4c653b6d347fb895ba3d950dfae81b65
Debash1sh-Panda <debasishpanda2036@gmail.com>
2024-05-30 20:17:29 +0530

    added files

---
+++ src/Components/Todo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Todo.css";
+
+function Todo({ todo, index, removeTodo }) {
+  return (
+    <div className="container">
+      <h3>YOUR TODO LIST</h3>
+      <div className="todo">
+        {todo.text}
+        <button onClick={() => removeTodo(index)}>X</button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
